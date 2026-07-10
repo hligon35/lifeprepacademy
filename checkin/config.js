4c10c8997a425f8a6b16a2e434ef83872f1a32ab
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-09 22:10:58 -0400

    Use hosted Fast Pass logo until local asset is deployed

--- checkin/config.js
+++ checkin/config.js
@@ -8,6 +8,6 @@ window.AT_THE_GATE_CONFIG = {
   registrationUrl: "",
   missingRegistrationUrl: "",
   googleAppsScriptUrl: "PASTE_DEPLOYED_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  fastPassLogoUrl: "fastpasslogo.png",
+  fastPassLogoUrl: "https://i.postimg.cc/0yvQYBxL/Paducah-Flag-Clinic-Collab.png",
   sheetIdHint: "16xbM_ZXe4mEdfjABwPVfc6HqWhn-iW9DumoFfaQ9JTQ"
 };
