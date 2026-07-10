d8104d81101070794c6d9f6bdcb758628a5a669d
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-07-09 21:57:18 -0400

    Set Paducah SMS schedule and local Fast Pass branding

--- checkin/config.js
+++ checkin/config.js
@@ -2,10 +2,12 @@ window.AT_THE_GATE_CONFIG = {
   appName: "Life Prep Fast Pass",
   eventName: "Paducah Flag Football Clinic",
   eventDate: "July 18, 2026",
+  eventTimeZone: "America/Chicago",
+  smsSendLocalTime: "2026-07-18T09:00:00-05:00",
   supportPhone: "",
   registrationUrl: "",
   missingRegistrationUrl: "",
   googleAppsScriptUrl: "PASTE_DEPLOYED_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE",
-  fastPassLogoUrl: "https://i.postimg.cc/0yvQYBxL/Paducah-Flag-Clinic-Collab.png",
+  fastPassLogoUrl: "fastpasslogo.png",
   sheetIdHint: "16xbM_ZXe4mEdfjABwPVfc6HqWhn-iW9DumoFfaQ9JTQ"
 };
