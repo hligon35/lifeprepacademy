4594158cc45f011090afc6bd6f8c81cee181e986
Harold Ligon <157246297+hligon35@users.noreply.github.com>
2026-08-07 15:49:40 -0400

    updated pdfs

--- mlsregistration/worker/pdf-field-maps.js
+++ mlsregistration/worker/pdf-field-maps.js
@@ -2,20 +2,20 @@ export const PLAYER_AGREEMENT_FIELD_MAP = {
   pageFromEnd: 1,
   defaultFontSize: 10,
   fields: {
-    participantNames: { x: 190, y: 632, maxWidth: 380, fontSize: 9 },
-    guardianName: { x: 150, y: 450, maxWidth: 300, fontSize: 10 },
-    guardianDob: { x: 480, y: 450, maxWidth: 95, fontSize: 10 },
-    guardianStreet: { x: 150, y: 423, maxWidth: 445, fontSize: 10 },
-    guardianCity: { x: 150, y: 395, maxWidth: 178, fontSize: 10 },
-    guardianState: { x: 340, y: 395, maxWidth: 74, fontSize: 10 },
-    guardianZip: { x: 462, y: 395, maxWidth: 105, fontSize: 10 },
-    guardianPhone: { x: 150, y: 367, maxWidth: 170, fontSize: 10 },
-    guardianEmail: { x: 336, y: 367, maxWidth: 232, fontSize: 10 },
-    signingDate: { x: 450, y: 339, maxWidth: 120, fontSize: 10 },
+    participantNames: { x: 75, y: 615, maxWidth: 380, fontSize: 9 },
+    guardianName: { x: 135, y: 450, maxWidth: 300, fontSize: 12 },
+    guardianDob: { x: 475, y: 450, maxWidth: 95, fontSize: 12 },
+    guardianStreet: { x: 135, y: 423, maxWidth: 445, fontSize: 12 },
+    guardianCity: { x: 135, y: 395, maxWidth: 178, fontSize: 12 },
+    guardianState: { x: 340, y: 395, maxWidth: 74, fontSize: 12 },
+    guardianZip: { x: 462, y: 395, maxWidth: 105, fontSize: 12 },
+    guardianPhone: { x: 135, y: 367, maxWidth: 170, fontSize: 12 },
+    guardianEmail: { x: 340, y: 367, maxWidth: 232, fontSize: 12 },
+    signingDate: { x: 450, y: 339, maxWidth: 120, fontSize: 12 },
   },
   signatureBounds: {
-    primary: { x: 148, y: 335, width: 238, height: 20 },
-    parent: { x: 148, y: 335, width: 238, height: 20 },
+    primary: { x: 135, y: 335, width: 238, height: 20 },
+    parent: { x: 135, y: 335, width: 238, height: 20 },
   },
 };
 
@@ -23,10 +23,10 @@ export const VOLUNTEER_AGREEMENT_FIELD_MAP = {
   pageFromEnd: 1,
   defaultFontSize: 10,
   fields: {
-    signingDate: { x: 125, y: 505, maxWidth: 120, fontSize: 10 },
-    legalName: { x: 125, y: 480, maxWidth: 250, fontSize: 10 },
+    signingDate: { x: 125, y: 503, maxWidth: 120, fontSize: 10 },
+    legalName: { x: 125, y: 476, maxWidth: 250, fontSize: 10 },
   },
   signatureBounds: {
-    primary: { x: 150, y: 445, width: 385, height: 34 },
+    primary: { x: 150, y: 440, width: 385, height: 34 },
   },
 };
